5d786b07d1ec9909dd440b153512cd6f4497a0be
mesterj <mesterj@freemail.hu>
2015-04-07 21:48:07 +0200

    third ver main commit

--- app/src/main/java/com/kite/joco/kitecrmdemo/CallEndReceiver.java
+++ app/src/main/java/com/kite/joco/kitecrmdemo/CallEndReceiver.java
@@ -7,41 +7,54 @@ import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Set;
 
 /**
  * Created by Joco on 2015.03.20..
  */
 public class CallEndReceiver extends BroadcastReceiver {
+    String filename = "CallerNum";
     @Override
     public void onReceive(Context context, Intent intent) {
+
+
 
         Bundle bundle = intent.getExtras();
 
         Set<String> keys = bundle.keySet();
-        final String TAG="KITECRMDEMO";
+        final String TAG = "KITECRMDEMO";
 
         /*
         The value of incoming_number key in the bundle is the called number, maybe passing for content of the new memo.
          */
 
-        TelephonyManager telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         CustomPhoneStateListener myStateListener = new CustomPhoneStateListener();
 
         telephony.listen(myStateListener, PhoneStateListener.LISTEN_CALL_STATE);
 
         String calledNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-        String callerNumber ="" ;
+
+        String callerNumber = loadConfig(context);
+
 
         String stateval = bundle.getString("state");
 
         // A hívás végének figyelése
-        if (stateval != null && stateval.equals("IDLE")){
-            Log.i(TAG,"CALL ENDED");
-            Log.i(TAG,stateval);
+        if (stateval != null && stateval.equals("IDLE")) {
+            Log.i(TAG, "CALL ENDED");
+            Log.i(TAG, stateval);
+            Log.i(TAG, "Most kellene hívni az activityt!");
+            Log.i(TAG, "callerNumber value is " + callerNumber);
             if (!callerNumber.equals("")) {
-                Intent newContactIntent = new Intent(context,NewContactActivity.class);
+                Intent newContactIntent = new Intent(context, NewContactActivity.class);
                 newContactIntent.putExtra("Number", callerNumber);
                 newContactIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(newContactIntent);
@@ -49,17 +62,42 @@ public class CallEndReceiver extends BroadcastReceiver {
 
             // Toast.makeText(context, "A hívott szám : " + calledNumber, Toast.LENGTH_LONG).show();
             // intent for create new memo
-        }
-        else if (stateval != null && stateval.equals("RINGING")){
-            Log.i(TAG,"CALL_STARTED");
-            Log.i(TAG,stateval);
-            Log.i(TAG+ " incoming number : ",bundle.getString("incoming_number"));
-            //Toast.makeText(context," A hívó száma: " + bundle.getString("incoming_number"),Toast.LENGTH_LONG).show();
-            callerNumber = bundle.getString("incoming_number");
-        }
-        else {
-            Log.i(TAG,"Nem tudom ilyenkor mi van");
+        } else if (stateval != null && stateval.equals("RINGING")) {
+            Log.i(TAG, "CALL_STARTED");
+            Log.i(TAG, stateval);
+            Log.i(TAG + " incoming number : ", bundle.getString("incoming_number"));
+            Toast.makeText(context, " A hívó száma: " + bundle.getString("incoming_number"), Toast.LENGTH_LONG).show();
+            FileOutputStream outputStream;
+            try {
+                outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(bundle.getString("incoming_number").getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Nem tudom ilyenkor mi van");
         }
 
+    }
+
+    private String loadConfig(Context context) {
+        try {
+            InputStream in = context.openFileInput(filename);
+            if (in != null) {
+                InputStreamReader tmp = new InputStreamReader(in);
+                BufferedReader reader = new BufferedReader(tmp);
+                String str;
+                StringBuilder buf = new StringBuilder();
+                while ((str = reader.readLine()) != null) {
+                    buf.append(str);
+                }
+                in.close();
+                return buf.toString();
+            }
+        } catch (IOException iex) {
+            return null;
+        }
+        return null;
     }
 }
--- app/src/main/java/com/kite/joco/kitecrmdemo/NewContactActivity.java
+++ app/src/main/java/com/kite/joco/kitecrmdemo/NewContactActivity.java
@@ -2,14 +2,29 @@ package com.kite.joco.kitecrmdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class NewContactActivity extends ActionBarActivity {
+
+    String callernumber="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
+        if (savedInstanceState != null){
+            callernumber = savedInstanceState.getString("Number");
+            TextView tvCallerNumber = (TextView) findViewById(R.id.tvHello);
+            tvCallerNumber.setText("A hívó száma ez volt: " + callernumber);
+        }
+    }
+
+    public void deleteNumber(View v){
+        final String connectfile = "CallerNum";
+        deleteFile(connectfile);
+        finish();
     }
 
 
